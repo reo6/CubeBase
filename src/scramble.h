cbaa838df24bfbd68ece9999ccc294c087aaa07e
reo6 <ramazanemre@protonmail.com>
2023-09-14 14:36:19 +0300

    Create definitions for scramble generation

--- src/scramble.h
+++ src/scramble.h
@@ -1,6 +1,24 @@
 #ifndef SCRAMBLE_H_
 #define SCRAMBLE_H_
 
+#include <stdbool.h>
 
+typedef enum {
+  R,
+  L,
+  U,
+  D,
+  F,
+  B
+} Notation;
+
+typedef struct {
+  Notation notation;
+  bool cclockwise;
+  bool doublemove;
+} Move; // TODO Find a better name
+
+Move random_move();
+Move* generate_scramble(int length);
 
 #endif // SCRAMBLE_H_
