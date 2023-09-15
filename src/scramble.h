a6e8ae4aed7078415a1be6cb52e0c3cbd2134d74
reo6 <ramazanemre@protonmail.com>
2023-09-15 20:27:24 +0300

    Finish the scramble generator

--- src/scramble.h
+++ src/scramble.h
@@ -1,26 +1,7 @@
 #ifndef SCRAMBLE_H_
 #define SCRAMBLE_H_
 
-#include <stdbool.h>
-
-typedef enum {
-  R,
-  L,
-  U,
-  D,
-  F,
-  B
-} Notation;
-
-typedef struct {
-  Notation notation;
-  bool cclockwise;
-  bool doublemove;
-} Move; // TODO Find a better name
-
-Move random_move();
-void generate_scramble(int length, Move* scramble);
-void move_to_string(Move move, char* move_str);
-void scramble_to_string(Move* scramble, char* scr_str, int length);
+int get_random(int min, int max);
+void generate_scramble(int length, char* scramble);
 
 #endif // SCRAMBLE_H_
