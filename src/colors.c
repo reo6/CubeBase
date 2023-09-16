8ad13cc0998fe3c02afe1b2e0344f92f2e1b2c0c
reo6 <ramazanemre@protonmail.com>
2023-09-16 16:43:03 +0300

    Add vintage color scheme

--- src/colors.c
+++ src/colors.c
@@ -6,6 +6,14 @@ const Color c_white = {0x00, 0x00, 0x00, 0xff};
 const Color c_darkgreen = {0x39, 0x51, 0x44, 0xff};
 const Color c_greenwhite = {0xf0, 0xeb, 0xce, 0xff};
 
+// #252B48
+const Color c_vintage_bg = {0x25, 0x2B, 0x48, 0xff};
+// #F7E987
+const Color c_vintage_fg = {0xF7, 0xE9, 0x87, 0xff};
+
+// Pre-defined color schemes:
+ColorScheme s_greenscheme;
+ColorScheme s_vintage;
 
 void scheme_init() {
     s_greenscheme = scheme_new(c_darkgreen, c_greenwhite, c_greenwhite);
--- src/colors.h
+++ src/colors.h
@@ -7,13 +7,22 @@ extern const Color c_black;
 extern const Color c_white;
 extern const Color c_darkgreen;
 extern const Color c_greenwhite;
-
+extern const Color c_vintage_bg;
+extern const Color c_vintage_fg;
 
 typedef struct {
   Color background;
   Color text;
   Color text_secondary;
 } ColorScheme;
+
+
+// Pre-defined color schemes:
+extern ColorScheme s_greenscheme;
+extern ColorScheme s_vintage;
+
+ColorScheme scheme_new(Color background, Color text, Color t2);
+void scheme_init();
 
 #endif // COLORS_H_
 
