652a7d7394d852f3ef058d71b7565fa5002618de
reo6 <ramazanemre@protonmail.com>
2023-09-16 16:17:46 +0300

    Use colors.h in main file

--- src/cubebase.c
+++ src/cubebase.c
@@ -5,21 +5,30 @@
 #include <raylib.h>
 #include "timer.h"
 #include "scramble.h"
+#include "colors.h"
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 #define TITLE "CubeBase"
 
+
 int main() {
     srand(time(NULL));
 
+    ColorScheme greenscheme;
+    greenscheme.background = c_darkgreen;
+    greenscheme.text = c_greenwhite;
+    greenscheme.text_secondary = c_greenwhite;
+
+    ColorScheme current_scheme = greenscheme;
+
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE);
-    Timer timer = timer_new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    Timer timer = timer_new(SCREEN_WIDTH, SCREEN_HEIGHT, &current_scheme);
 
     while (!WindowShouldClose()) {
         timer_update(&timer);
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground(current_scheme.background);
         timer_draw(&timer);
         EndDrawing();
     }
